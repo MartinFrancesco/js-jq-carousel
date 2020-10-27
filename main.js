529eb806b80993d2d35f2373fb2560914d7da991
Francesco <francescomartinmail@gmail.com>
2020-10-27 16:49:25 +0100

    Exercise

--- main.js
+++ main.js
@@ -1,1 +1,52 @@
-console.log('Init');
+/**
+* JQUERY
+**/
+
+$( document ).ready(function() {
+
+   //CLICK
+   $ ('.prev').click( function() {
+     nextPrevImages('prev');
+   });
+
+   $ ('.next').click( function() {
+     nextPrevImages('next');
+   });
+
+
+}); //Doc ready!
+
+//Dichiarazione funzione
+function nextPrevImages(direction) {
+
+  var activeImage = $ ('.images img.active');
+  var activeCircle = $ ('nav i.active');
+
+  //Reset
+  activeImage.removeClass('active');
+  activeCircle.removeClass('active');
+
+  //NEXT
+  if (direction === 'next') {
+
+    if (activeImage.hasClass('last') === true) {
+      $ ('.images img.first').addClass('active');
+      $ ('nav i.first').addClass('active');
+    } else {
+      activeImage.next('img').addClass('active');
+      activeCircle.next('i').addClass('active');
+    }
+  }
+
+  //PREV
+  else if (direction === 'prev') {
+
+    if (activeImage.hasClass('first') === true) {
+      $ ('.images img.last').addClass('active');
+      $ ('nav i.last').addClass('active');
+    } else {
+      activeImage.prev('img').addClass('active');
+      activeCircle.prev('i').addClass('active');
+    }
+  }
+}
